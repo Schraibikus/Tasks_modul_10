e8d0e7e1ff3c6bd646576f42a33621fccbc2ed12
Schraibikus <kurt-77@mail.ru>
2023-12-16 16:42:01 +0500

    tast 1 added a check for an empty string

--- task_4.js
+++ task_4.js
@@ -2,7 +2,7 @@
 // Используйте объект Math.
 console.log('Задача 4');
 let value = Math.floor(Math.random() * 101);
-console.log(value);
+console.log(`Случайное число от 0 до 100: ${value}`);
 console.log('\n');
 
 
